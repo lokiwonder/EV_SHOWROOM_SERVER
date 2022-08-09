6da8b5670063618f04d3d6e1faf7f28e3ec59d90
loki <loki@wondermove.net>
2022-08-10 01:12:57 +0900

    2022.08.10 01:12

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const serverConfig = config.get('server');
   const port = serverConfig.port;
